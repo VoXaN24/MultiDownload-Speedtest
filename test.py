e1dbb86ad0df76a46bd87b460ef3edd64f83064b
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2022-12-16 14:07:57 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -3,5 +3,6 @@ from tqdm import tqdm
 url=open('url.txt').read().splitlines()
 with open('result.txt', 'w') as f:
 	for i in tqdm(range(len(url))):
-		out=popen(["wget","-O","/dev/null",url[i],"2>&1","|","grep","-o",'"[0-9.]\+','[KMG]*B/s"']).read()
+		args = ["wget","-O","/dev/null",url[i],"2>&1","|","grep","-o",'"[0-9.]\+','[KMG]*B/s"']
+		out=popen(' '.join(args)).read()
 		f.write(f"{url[i]} have for speed {out}\n")
