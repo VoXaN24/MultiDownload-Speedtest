91f6b8dd2742195191530d6fa666fc2df50a4c6d
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2022-12-16 14:02:10 +0100

    Debug

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 url=open('url.txt').read().splitlines()
 with open('result.txt', 'w') as f:
 	for i in tqdm(range(len(url))):
-		proc=subprocess.Popen(["wget","-O","/dev/null",url[i],"2>&1","|","grep","-o",'"[0-9.]\+','[KMG]*B/s"'], stdout=subprocess.PIPE, shell=True)
+		command="wget -O /dev/null "+url[i]+' 2>&1 grep -o "[0-9.]\+ [KMG]*B/s"'
+		proc=subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
 		(out,err) = proc.communicate()
 		f.write(url[i]+" have for speed "+out+"\n")
