5cdece197ec4da6e6471808f93acef2e043dccc0
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2022-12-16 14:00:21 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ with open('result.txt', 'w') as f:
 	for i in tqdm(range(len(url))):
 		proc=subprocess.Popen(["wget","-O","/dev/null",url[i],"2>&1","|","grep","-o",'"[0-9.]\+','[KMG]*B/s"'], stdout=subprocess.PIPE, shell=True)
 		(out,err) = proc.communicate()
-		f.write(f"{url[i]} have for speed {out}\n")
+		f.write(url[i]+" have for speed "+out+"\n")
