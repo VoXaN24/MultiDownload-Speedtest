2bca506f9c0c198c27a7a27a91469a4d2b1578ca
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2022-12-16 14:06:11 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
-import subprocess
+from os import popen
 from tqdm import tqdm
 url=open('url.txt').read().splitlines()
 with open('result.txt', 'w') as f:
 	for i in tqdm(range(len(url))):
-		command="wget -O /dev/null "+url[i]+' 2>&1 grep -o "[0-9.]\+ [KMG]*B/s"'
-		proc=subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-		(out,err) = proc.communicate()
-		f.write(url[i]+" have for speed "+str(out)+"\n")
+		out=popen(["wget","-O","/dev/null",url[i],"2>&1","|","grep","-o",'"[0-9.]\+','[KMG]*B/s"']).read()
+		f.write(f"{url[i]} have for speed {out}\n")
