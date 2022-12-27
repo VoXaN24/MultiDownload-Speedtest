70e2a7dfa5509c86143aa4a61000a0e72376b225
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2022-12-27 19:13:18 +0100

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from os import popen
-from tqdm import tqdm
-url=open('url.txt').read().splitlines()
-with open('result.txt', 'w') as f:
-	for i in tqdm(range(len(url))):
-		args = ["wget","-O","/dev/null",url[i],"2>&1","|","grep","-o",'"[0-9.]\+','[KMG]*B/s"']
-		out=popen(' '.join(args)).read()
-		f.write(f"{url[i]} have for speed {out}\n")
